4b9d5e2ad84294954b6c473c2bd780f08f5f8b3e
DemetrioPadre <demetriopadre@gmail.com>
2024-02-21 09:20:36 +0100

    add box shadow (no good)

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -9,12 +9,14 @@ export default {
     <div class="footerContainer">
         <div class="footerNav d-flex justify-content-center">
             <div class="navShadow  ">
+
                 <div class="leftSideBar d-flex ">
+
                     <form class="d-flex " role="search">
                         <input class="searchBar form-control rounded-0" type="search"
                             placeholder="Subscribe to our newsletter" aria-label="Search">
                     </form>
-                    <button class="btn btn-danger rounded-0" type="submit">Register</button>
+                    <button class="buttn btn btn-danger rounded-0" type="submit">Register</button>
                 </div>
 
                 <div class="rightSideBar ">
@@ -39,11 +41,31 @@ export default {
     display: flex;
     justify-content: space-between;
     margin-top: 100px;
+    box-shadow: inset 20px 50px 50px 5px #172331;
+    padding: 30px;
+    align-items: center;
+
 }
 
+
 .searchBar {
-    width: 300px;
 
+    height: 50px;
+    color: lightgrey;
+}
 
+.leftSideBar {
+    border-left: 7px solid #23374b;
+
+}
+
+// .clbar {
+//     width: 7px;
+//     height: 60px;
+
+// }
+
+.buttn {
+    height: 50px;
 }
 </style>
