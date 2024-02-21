f000b99c021088a17dc43a5959b39053907ff554
DemetrioPadre <demetriopadre@gmail.com>
2024-02-21 08:50:20 +0100

    add nav bar

--- src/components/AppCarousel.vue
+++ src/components/AppCarousel.vue
@@ -13,7 +13,7 @@ export default {
 </script>
 <template>
     <div class="col-3 mt-5">
-        <div class="card d-flex">
+        <div class="card d-flex border-0">
             <img class="card-img-top" :src="generateURL('../assets/img/' + item.image)" alt="">
         </div>
     </div>
--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -8,12 +8,21 @@ export default {
 <template>
     <div class="footerContainer">
         <div class="footerNav d-flex justify-content-center">
-            <div class="navShadow d-flex justify-content-center">
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Subscribe to our newsletter"
-                        aria-label="Search">
-                </form>
-                <button class="btn btn-danger" type="submit">Register</button>
+            <div class="navShadow  ">
+                <div class="leftSideBar d-flex ">
+                    <form class="d-flex " role="search">
+                        <input class="searchBar form-control rounded-0" type="search"
+                            placeholder="Subscribe to our newsletter" aria-label="Search">
+                    </form>
+                    <button class="btn btn-danger rounded-0" type="submit">Register</button>
+                </div>
+
+                <div class="rightSideBar ">
+                    <span><span>Insights is Picwik</span>University's thought leadership publication for sharing <br>
+                        knowledge
+                        on managment, technology and innovation.</span>
+                    <p><strong>Take me there</strong><i class="fa-solid fa-angle-right"></i></p>
+                </div>
             </div>
 
         </div>
@@ -22,7 +31,19 @@ export default {
 </template>
 <style lang="scss">
 .footerContainer {
-    height: 1000px;
+    height: 800px;
     background-color: #192735;
+}
+
+.navShadow {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 100px;
+}
+
+.searchBar {
+    width: 300px;
+
+
 }
 </style>
